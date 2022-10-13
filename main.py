712bbc04b67f152dc4ee86a4da8b2eae7dc1d3d0
ginsuast <ginsuast@proteccion.com.co>
2022-10-13 10:39:44 -0500

    lectura de archivo. FIN INTRODUCCION

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ import re
 URL = "http://econpy.pythonanywhere.com/ex/001.html"
 
 if __name__ == '__main__':
-    response = requests.get(URL)
-    if response.status_code == 200:
-        content = response.text
-        
+    with open('econpy.html','r') as file:
+        content = file.read()
         regex = '<div title="buyer-name">(.+?)</div>'
         matches = re.findall(regex, content)
         for title in matches:
