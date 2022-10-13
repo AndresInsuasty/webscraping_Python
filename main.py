23318bc98c94dd387b374cfac0227d38107c9d1a
ginsuast <ginsuast@proteccion.com.co>
2022-10-13 15:47:33 -0500

    visitar dos paginas web con Selenium

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-import requests
-import re
+import unittest
+from pyunitreport import HTMLTestRunner
+from selenium import webdriver
 
-URL = "http://econpy.pythonanywhere.com/ex/001.html"
+class HelloWorld(unittest.TestCase):
+    @classmethod # Decorador para que las distintas paginas corran en una sola pestaña
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(executable_path=r"./chromedriver.exe")
+        driver = cls.driver
+		# esperamos 10 seg antes de realizar la siguiente accion
+        driver.implicitly_wait(10)
 
-if __name__ == '__main__':
-    with open('econpy.html','r') as file:
-        content = file.read()
-        regex = '<div title="buyer-name">(.+?)</div>'
-        matches = re.findall(regex, content)
-        for title in matches:
-            print(title)
+	# Caso de prueba donde realizaremos una serie de acciones para que el navegador las automatice
+    def test_hello_world(self):
+        driver = self.driver
+        driver.get('https://www.platzi.com')
+
+    def test_visit_rues(self):
+        self.driver.get('https://www.rues.org.co')
+        
+		
+	# Cerramos el navegador una vez terminadas las pruebas
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+if __name__ == "__main__":
+	unittest.main(verbosity = 2, testRunner = HTMLTestRunner(output = 'reportes', report_name = 'hello-world-report'))
